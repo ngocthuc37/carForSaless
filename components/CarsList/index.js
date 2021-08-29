4a48bbc18a83b5f99fb17b77a10e32b4f60098d7
Thuc Trieu <trieumaingocthuc37@gmail.com>
2021-08-29 10:06:04 +0700

    fixed bug, finish CarList components

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import {View,Text, ImageBackground, FlatList} from 'react-native';
+import {View,Text, Dimensions, FlatList} from 'react-native';
+
 import styles from './styles';
-import CarItem from '../car';
+import CarItem from "../car";
 import cars from './cars';
 
 const CarsList = (props) => {
 
-    console.log(cars);
+    
 
     return (
-    <View style ={styles.container}>
+    <View style={styles.container}>
         <FlatList
             data={cars}
-            renderItem={({item}) => <CarItem car={item} /> } 
+            renderItem={({item}) => <CarItem car={item} />}
+            showsVerticalScrollIndicator={false}
+            snapToAlignment={'start'}
+            decelerationRate={'fast'}
+            snapToInterval={Dimensions.get('window').height}
         />
     </View>    
     );
--- components/CarsList/styles.js
+++ components/CarsList/styles.js
@@ -1,7 +1,6 @@
 import { StyleSheet } from "react-native";
  
 const styles = StyleSheet.create({
-
       container: {
         width: '100%',
       },
